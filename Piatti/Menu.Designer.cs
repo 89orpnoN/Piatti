cada5ecd6ed56c146c5d52564fb621b79ced6fd3
89orpnoN <9698022@itisrossi.vi.it>
2023-10-13 12:01:06 +0200

    primo commit dell'asporto

---
+++ Piatti/Menu.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Piatti
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SecondoPiattoBox = new ComboBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            groupBox3 = new GroupBox();
+            DessertBox = new ComboBox();
+            groupBox2 = new GroupBox();
+            PrimoPiattoBox = new ComboBox();
+            groupBox1 = new GroupBox();
+            DessertPicture = new PictureBox();
+            SecondoPiattoPicture = new PictureBox();
+            PrimoPiattoPicture = new PictureBox();
+            tableLayoutPanel1.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)DessertPicture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)SecondoPiattoPicture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PrimoPiattoPicture).BeginInit();
+            SuspendLayout();
+            // 
+            // SecondoPiattoBox
+            // 
+            SecondoPiattoBox.Anchor = AnchorStyles.None;
+            SecondoPiattoBox.FormattingEnabled = true;
+            SecondoPiattoBox.Location = new Point(44, 14);
+            SecondoPiattoBox.Name = "SecondoPiattoBox";
+            SecondoPiattoBox.Size = new Size(150, 23);
+            SecondoPiattoBox.TabIndex = 2;
+            SecondoPiattoBox.Text = "Niente";
+            SecondoPiattoBox.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            SecondoPiattoBox.TextChanged += SecondoPiattoBox_TextChanged;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33333F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.1511841F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.5154839F));
+            tableLayoutPanel1.Controls.Add(groupBox3, 2, 0);
+            tableLayoutPanel1.Controls.Add(groupBox2, 0, 0);
+            tableLayoutPanel1.Controls.Add(groupBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(DessertPicture, 2, 1);
+            tableLayoutPanel1.Controls.Add(SecondoPiattoPicture, 1, 1);
+            tableLayoutPanel1.Controls.Add(PrimoPiattoPicture, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(DessertBox);
+            groupBox3.Location = new Point(534, 3);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(200, 44);
+            groupBox3.TabIndex = 4;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Dessert";
+            // 
+            // DessertBox
+            // 
+            DessertBox.Anchor = AnchorStyles.None;
+            DessertBox.FormattingEnabled = true;
+            DessertBox.Location = new Point(27, 15);
+            DessertBox.Name = "DessertBox";
+            DessertBox.Size = new Size(150, 23);
+            DessertBox.TabIndex = 4;
+            DessertBox.Text = "Niente";
+            DessertBox.SelectedIndexChanged += DessertBox_SelectedIndexChanged;
+            DessertBox.TextUpdate += DessertBox_TextUpdate;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(PrimoPiattoBox);
+            groupBox2.Location = new Point(3, 3);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 44);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Primo Piatto";
+            // 
+            // PrimoPiattoBox
+            // 
+            PrimoPiattoBox.Anchor = AnchorStyles.None;
+            PrimoPiattoBox.FormattingEnabled = true;
+            PrimoPiattoBox.Location = new Point(15, 18);
+            PrimoPiattoBox.Name = "PrimoPiattoBox";
+            PrimoPiattoBox.Size = new Size(150, 23);
+            PrimoPiattoBox.TabIndex = 3;
+            PrimoPiattoBox.Text = "Niente";
+            PrimoPiattoBox.TextChanged += PrimoPiattoBox_TextChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(SecondoPiattoBox);
+            groupBox1.Location = new Point(269, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 44);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Secondo Piatto";
+            // 
+            // DessertPicture
+            // 
+            DessertPicture.Dock = DockStyle.Fill;
+            DessertPicture.Location = new Point(534, 63);
+            DessertPicture.Name = "DessertPicture";
+            DessertPicture.Size = new Size(263, 384);
+            DessertPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+            DessertPicture.TabIndex = 7;
+            DessertPicture.TabStop = false;
+            // 
+            // SecondoPiattoPicture
+            // 
+            SecondoPiattoPicture.Dock = DockStyle.Fill;
+            SecondoPiattoPicture.Location = new Point(269, 63);
+            SecondoPiattoPicture.Name = "SecondoPiattoPicture";
+            SecondoPiattoPicture.Size = new Size(259, 384);
+            SecondoPiattoPicture.TabIndex = 5;
+            SecondoPiattoPicture.TabStop = false;
+            SecondoPiattoPicture.Click += SecondoPiattoPicture_Click;
+            // 
+            // PrimoPiattoPicture
+            // 
+            PrimoPiattoPicture.Dock = DockStyle.Fill;
+            PrimoPiattoPicture.Location = new Point(3, 63);
+            PrimoPiattoPicture.Name = "PrimoPiattoPicture";
+            PrimoPiattoPicture.Size = new Size(260, 384);
+            PrimoPiattoPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+            PrimoPiattoPicture.TabIndex = 6;
+            PrimoPiattoPicture.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            MinimumSize = new Size(650, 200);
+            Name = "Form1";
+            Text = "Scegli il tuo menù";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)DessertPicture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)SecondoPiattoPicture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PrimoPiattoPicture).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private ComboBox SecondoPiattoBox;
+        private TableLayoutPanel tableLayoutPanel1;
+        private ComboBox DessertBox;
+        private ComboBox PrimoPiattoBox;
+        private PictureBox SecondoPiattoPicture;
+        private PictureBox DessertPicture;
+        private PictureBox PrimoPiattoPicture;
+        private GroupBox groupBox1;
+        private GroupBox groupBox3;
+        private GroupBox groupBox2;
+    }
+}
---
+++ Piatti/MenuAsporto.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Piatti
+{
+    partial class MenuAsporto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            MenuSummon = new Button();
+            MenuAsportoSummon = new Button();
+            TrattoriaWall = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)TrattoriaWall).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(TrattoriaWall);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tableLayoutPanel1);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 266;
+            splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(530, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(MenuSummon, 0, 1);
+            tableLayoutPanel2.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 3);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(259, 444);
+            tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 1;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Controls.Add(MenuAsportoSummon, 0, 1);
+            tableLayoutPanel3.Controls.Add(pictureBox2, 0, 0);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(268, 3);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 2;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Size = new Size(259, 444);
+            tableLayoutPanel3.TabIndex = 1;
+            // 
+            // MenuSummon
+            // 
+            MenuSummon.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            MenuSummon.Location = new Point(3, 321);
+            MenuSummon.Name = "MenuSummon";
+            MenuSummon.Size = new Size(253, 23);
+            MenuSummon.TabIndex = 0;
+            MenuSummon.Text = "Magno In sede";
+            MenuSummon.UseVisualStyleBackColor = true;
+            // 
+            // MenuAsportoSummon
+            // 
+            MenuAsportoSummon.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            MenuAsportoSummon.Location = new Point(3, 321);
+            MenuAsportoSummon.Name = "MenuAsportoSummon";
+            MenuAsportoSummon.Size = new Size(253, 23);
+            MenuAsportoSummon.TabIndex = 1;
+            MenuAsportoSummon.Text = "Magno a casa";
+            MenuAsportoSummon.UseVisualStyleBackColor = true;
+            // 
+            // TrattoriaWall
+            // 
+            TrattoriaWall.BorderStyle = BorderStyle.FixedSingle;
+            TrattoriaWall.Dock = DockStyle.Fill;
+            TrattoriaWall.Location = new Point(0, 0);
+            TrattoriaWall.Name = "TrattoriaWall";
+            TrattoriaWall.Size = new Size(266, 450);
+            TrattoriaWall.TabIndex = 0;
+            TrattoriaWall.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(253, 216);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Dock = DockStyle.Fill;
+            pictureBox2.Location = new Point(3, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(253, 216);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // MenuAsporto
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
+            Name = "MenuAsporto";
+            Text = "MenuAsporto";
+            Load += MenuAsporto_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)TrattoriaWall).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private PictureBox TrattoriaWall;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button MenuSummon;
+        private TableLayoutPanel tableLayoutPanel3;
+        private Button MenuAsportoSummon;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
